66303b6cc3297086cdc896660e31f2046ea98abd
Akrati <singhakrati0101@gmail.com>
2025-08-26 20:54:45 +0530

    feat: Implement user authentication and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,43 @@
-import { data } from 'autoprefixer'
-import React from 'react'
-import { Routes } from 'react-router'
+import { Navigate, Route, Routes } from "react-router";
 
-function App() {
+import HomePage from "./pages/Home.jsx";
+import SignUpPage from "./pages/SignUp.jsx";
+import LoginPage from "./pages/Login.jsx";
+import NotificationsPage from "./pages/Notifications.jsx";
+import CallPage from "./pages/VideoCall.jsx";
+import ChatPage from "./pages/Chat.jsx";
+import OnboardingPage from "./pages/Bio.jsx";
+
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
+
+
+const App = () => {
+  
+  const {data: authData , isLoading , error} =useQuery({
+    queryKey: ['authUser'],
+    queryFn: async ()=>{
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry : false, 
+  });
+  const authUser = authData?.user || null;
   return (
-    <div className= "text-5xl" data-theme = "forest">
+    <div className="h-screen" data-theme="night">
       <Routes>
-        
+        <Route path = "/" element = {authUser? <HomePage/> : <Navigate to = "/login" />}/>
+        <Route path = "/signup" element = {authUser? <Navigate to = "/" /> : <SignUpPage/>}/>
+        <Route path = "/login" element = {authUser? <Navigate to = "/" /> : <LoginPage/>}/>
+        <Route path = "/notifications" element = {authUser? <NotificationsPage/> : <Navigate to = "/login" />}/>
+        <Route path = "/call" element = {authUser? <CallPage/> : <Navigate to = "/login " />}/>
+        <Route path = "/chat/:chatId" element = {authUser? <ChatPage/> : <Navigate to = "/login " />}/>
+        <Route path = "/onboarding" element = {authUser? <OnboardingPage/> : <Navigate to = "/login " />}/>
       </Routes>
-    </div>
-  )
-}
 
-export default App
+      <Toaster />
+    </div>
+  );
+};
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+//useQuery when get req
+//mutation when post,put,delete req
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
