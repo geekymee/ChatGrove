fbefa8877a8361a962dccc60ad0b47a4d0664287
Akrati <singhakrati0101@gmail.com>
2025-08-30 23:15:11 +0530

    fix: correct import path for Chat model in chat.controller.js

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import Chat from "../models/chat.js";
+import Chat from "../models/Chat.js";
 import { generateStreamToken } from "../lib/stream.js";
 
 import cloudinary from "../lib/cloudinary.js";
