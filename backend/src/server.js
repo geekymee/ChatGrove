21da0c834cb250aa245374c65ceded30115a1245
Akrati <singhakrati0101@gmail.com>
2025-08-30 22:52:51 +0530

    initialize package.json for ChatGrove project with build and start scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,11 +6,13 @@ import chatRoutes from './routes/chat.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 import {app , server} from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -25,6 +27,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chats", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
